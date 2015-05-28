a4264c6620440a7313eebb216dda9df2c15075a1
smartapant <smartapant@gmail.com>
2015-05-28 15:56:02 +0300

    Noncordova login returned

--- www/modules/auth/login.js
+++ www/modules/auth/login.js
@@ -4,18 +4,35 @@
 (function(angular) {
     angular.module('app.auth')
         .controller('DeviceLoginController', DeviceLoginController)
+        .factory('vkAuthenticator', vkAuthenticator)
         .service('vkApi', vkApi);
 
-    DeviceLoginController.$inject = ['$scope', '$rootScope', '$state', 'vkApi', 'userApi'];
-    function DeviceLoginController($scope, $rootScope, $state, vkApi, userApi) {
+    DeviceLoginController.$inject = ['$scope', '$rootScope', '$state', 'vkApi', 'userApi', 'vkAuthenticator'];
+    function DeviceLoginController($scope, $rootScope, $state, vkApi, userApi, vkAuthenticator) {
         $scope.isAndroid = ionic.Platform.isAndroid();
 
         $scope.$onVkSdkEvent('vkSdk.newToken', function(evt) {
-            userApi
-                .login({
-                    user_id: evt.detail.userId,
-                    access_token: evt.detail.accessToken
-                })
+            afterTokenReceive({
+                user_id: evt.detail.userId,
+                access_token: evt.detail.accessToken
+            });
+        });
+
+        $scope.doAuthenticate = function() {
+            if (window.cordova) {
+                vkApi.initiateLogin(['photos', 'offline']);
+            } else {
+                vkAuthenticator()
+                    .then(function(authParams) {
+                        return afterTokenReceive(authParams);
+                    })
+            }
+
+        };
+
+        function afterTokenReceive(params) {
+            return userApi
+                .login(params)
                 .then(function() {
                     $rootScope.$broadcast('user.login');
                     $state.go('main.swiper');
@@ -23,18 +40,16 @@
                     // TODO: User friendly error
                     alert('Service temporary unavailable. Please try again later');
                 });
-        });
-
-        $scope.doAuthenticate = function() {
-            vkApi.initiateLogin(['photos', 'offline']);
-        };
+        }
     }
 
     vkApi.$inject = ['$rootScope', '$ionicPlatform'];
     function vkApi($rootScope, $ionicPlatform) {
         $ionicPlatform.ready(function() {
-            var VkSdk = window.VkSdk;
-            VkSdk.init('4851553');
+            if (window.cordova) {
+                var VkSdk = window.VkSdk;
+                VkSdk.init('4851553');
+            }
         });
 
         this.initiateLogin = function(permissions) {
@@ -46,6 +61,32 @@
             this.$on('$destroy', function() {
                 document.removeEventListener(eventName, callback);
             });
+        };
+    }
+
+    vkAuthenticator.$inject = ['$q', '$window', 'appUtilities'];
+    function vkAuthenticator($q, $window, appUtilities) {
+        return function() {
+            var resDefer = $q.defer();
+
+            var authURL = "https://oauth.vk.com/authorize?client_id=4851553&scope=photos&redirect_uri=http://oauth.vk.com/blank.html&display=touch&response_type=token";
+            var wwwref = $window.open(encodeURI(authURL), '_blank', 'location=no');
+            wwwref.addEventListener('loadstop', onLoadStop);
+            var callbackFunctionName = 'vkAuthenticatorCallback_' + new Date().getTime();
+            var callbackFunction = $window[callbackFunctionName] = function(queryStr) {
+                delete $window[callbackFunctionName];
+                resDefer.resolve(appUtilities.queryToObject(queryStr));
+            };
+
+            return resDefer.promise;
+
+            function onLoadStop(event) {
+                var tmp = (event.url).split("#");
+                if (tmp[0] == 'https://oauth.vk.com/blank.html' || tmp[0] == 'http://oauth.vk.com/blank.html') {
+                    wwwref.close();
+                    callbackFunction(tmp[1]);
+                }
+            }
         };
     }
 
