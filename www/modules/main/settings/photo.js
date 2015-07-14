3e66f4a032ffaedd340a9673ae7448367e58c88f
smartapant <smartapant@gmail.com>
2015-07-14 14:31:11 +0300

    Photo settings grid

--- www/modules/main/settings/photo.js
+++ www/modules/main/settings/photo.js
@@ -1,11 +1,33 @@
 (function(angular) {
 
     angular.module('app.main.settings')
+        .service('photoSettingsWidthCalculator', photoSettingsWidthCalculator)
         .controller('PhotoSettingsController', PhotoSettingsController);
 
-    PhotoSettingsController.$inject = ['$scope', 'userPhotos'];
-    function PhotoSettingsController($scope, userPhotos) {
+    PhotoSettingsController.$inject = ['$scope', 'userPhotos', 'photoSettingsWidthCalculator'];
+    function PhotoSettingsController($scope, userPhotos, photoSettingsWidthCalculator) {
         $scope.photos = userPhotos;
+        $scope.photosWidth = photoSettingsWidthCalculator.calculate();
+        console.log($scope.photosWidth);
+        console.log(userPhotos);
+    }
+
+    photoSettingsWidthCalculator.$inject = [];
+    function photoSettingsWidthCalculator() {
+        var screenWidth = screen.width;
+        var containerPaddings = 10;
+        var photoGutter = 15;
+
+        this.calculate = function() {
+            var contentWidth = screenWidth - containerPaddings*2;
+            var smallPhotoWidth = (contentWidth - photoGutter*2)/3;
+            var bigPhotoWidth = contentWidth - smallPhotoWidth - photoGutter;
+
+            return {
+                smallPhotoWidth: smallPhotoWidth,
+                bigPhotoWidth: bigPhotoWidth
+            }
+        }
     }
 
 
