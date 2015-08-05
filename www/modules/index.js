f75d4777e1dddf96be740e3a329f0a039338a371
smartapant <smartapant@gmail.com>
2015-08-05 15:04:58 +0300

    Ios keyboard scroll bug.

--- www/modules/index.js
+++ www/modules/index.js
@@ -38,7 +38,7 @@
             // for form inputs)
             if (window.cordova && window.cordova.plugins.Keyboard) {
                 cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-                // cordova.plugins.Keyboard.disableScroll(true);
+                cordova.plugins.Keyboard.disableScroll(true);
             }
             if (window.StatusBar) {
                 // org.apache.cordova.statusbar required
