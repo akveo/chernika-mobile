afb1fdad35858d5f559dbf922a43cb58da1b69d5
smartapant <smartapant@gmail.com>
2015-09-07 14:07:40 +0300

    offline access for pc

--- www/modules/auth/login.js
+++ www/modules/auth/login.js
@@ -76,7 +76,7 @@
         return function() {
             var resDefer = $q.defer();
 
-            var authURL = "https://oauth.vk.com/authorize?client_id=4851553&scope=photos&redirect_uri=http://oauth.vk.com/blank.html&display=touch&response_type=token";
+            var authURL = "https://oauth.vk.com/authorize?client_id=4851553&scope=photos,offline&redirect_uri=http://oauth.vk.com/blank.html&display=touch&response_type=token";
             var wwwref = $window.open(encodeURI(authURL), '_blank', 'location=no');
             wwwref.addEventListener('loadstop', onLoadStop);
             var callbackFunctionName = 'vkAuthenticatorCallback_' + new Date().getTime();
