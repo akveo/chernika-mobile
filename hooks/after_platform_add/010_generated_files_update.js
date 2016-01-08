f6076b7737eb33759e4fa719a8457192ba4ba3d9
Vladimir Lugovsky <lugovsky.v@gmail.com>
2016-01-08 18:18:57 +0300

    Ios build fixes

--- hooks/after_platform_add/010_generated_files_update.js
+++ hooks/after_platform_add/010_generated_files_update.js
@@ -7,7 +7,8 @@ var projectPropsPathUpd = '\nandroid.library.reference.2=vksdk_library';
 
 var buildGradlePath = 'platforms/android/build.gradle';
 var buildGradleUpd = "\nconfigurations { all*.exclude group: 'com.android.support', module: 'support-v4' }";
-
+if (fs.existsSync(projectPropsPath))
 fs.appendFileSync(projectPropsPath, projectPropsPathUpd);
 
+if (fs.existsSync(buildGradlePath))
 fs.appendFileSync(buildGradlePath, buildGradleUpd);
